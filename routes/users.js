8fe808f1cf03f895c683fc336a0f425965fa937b
Urian Muro <urianmuro@Urians-MacBook-Pro.local>
2021-03-29 02:13:49 -0700

    set

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,15 @@
 var express = require('express');
 var router = express.Router();
 
+const isLoggedIn = require('../config/auth');
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/new', isLoggedIn, usersCtrl.new);
+router.get('/:id', lendingCtrl.show);
+router.post('/', isLoggedIn, usersCtrl.create);
 
 module.exports = router;
