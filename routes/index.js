9f5dbe43b8695c9ccc84c50b19a2f37fce11153e
Urian Muro <urianmuro@Urians-MacBook-Pro.local>
2021-03-31 10:19:11 -0700

    nodemon fired, GET/500err

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const passport = require('passport');
 
 router.get('/', function(req, res, next) {
-  res.render('views');
+  res.render('index');
 });
 
 router.get('/auth/google', passport.authenticate(
